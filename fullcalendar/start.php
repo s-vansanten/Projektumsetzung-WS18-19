dd9dffbc58023ba39f4073d6ffbda7f177e335e7
Spoing01 <simon-vs@posteo.de>
2018-10-20 12:38:12 +0200

    Anpassung Beispiel Kalender + Nutzung get-events.php
    Beispiel Kalender start.php wurde angepasst
    Es wird get-events.php aus dem demo-Ordner zur Einlesung von Events aus einer json-Datei genutzt

--- fullcalendar/start.php
+++ fullcalendar/start.php
@@ -13,27 +13,53 @@
 			header:{
 				left:'prev,next today',
 				center:'title',
-				right:'month,agendaWeek,agendaDay'
+				right:'month,agendaWeek,agendaDay,listWeek'
 			},
-			defaultView: 'agendaWeek',
-			events: [
-				{
-					title: 'Test',
-					start: '2018-10-17T10:00:00+0100',
-					end: '2018-10-17T14:00:00+0100',
-				},
-				{
-					title: 'Test2',
-					start: '2018-10-17T14:00:00+0100',
-					end: '2018-10-17T18:00:00+0100',
-				}
-			],
+			events: {
+					url: 'php/get-events.php',
+					error: function() {
+						$('#script-warning').show();
+					}
+			},
 			selectable:true,
 			selectHelper:true,
 			
 		})
 		});
 		</script>
+		<style>
+			body {
+			margin: 0;
+			padding: 0;
+			font-family: "Lucida Grande",Helvetica,Arial,Verdana,sans-serif;
+			font-size: 14px;
+		  }
+
+		  #script-warning {
+			display: none;
+			background: #eee;
+			border-bottom: 1px solid #ddd;
+			padding: 0 10px;
+			line-height: 40px;
+			text-align: center;
+			font-weight: bold;
+			font-size: 12px;
+			color: red;
+		  }
+
+		  #loading {
+			display: none;
+			position: absolute;
+			top: 10px;
+			right: 10px;
+		  }
+
+		  #calendar {
+			max-width: 900px;
+			margin: 40px auto;
+			padding: 0 10px;
+		  }
+		</style>
 	</head>
 	
 	<body>
