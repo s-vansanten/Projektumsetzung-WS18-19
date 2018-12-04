a53aaf083af2cac641840a44aad099f67c162674
s-vansanten <s_vansanten@stud.hwr-berlin.de>
2018-12-05 00:04:59 +0100

    Admin Portal - Freigabe Kalender hinzugefügt

--- admin-portal/admin-portal-v2.php
+++ admin-portal/admin-portal-v2.php
@@ -1,59 +1,199 @@
 <!DOCTYPE HTML>
 <html>
 <head>
-<link href='style.css' rel='stylesheet' />
+<link href='../admin/style/style.css' rel='stylesheet' />
 <title>Admin-Portal der HWR</title>
 <script>
-
+	
 </script>
 </head>
 <body>
 
 
-<figure> <img src="https://upload.wikimedia.org/wikipedia/de/9/90/Hochschule_f%C3%BCr_Wirtschaft_und_Recht_Berlin_logo.svg" width="604" height="120"
-	alt="Logo">
-	
-</figure>
+
 <form action="admin-portal-login.php" method="post" id="logout">
     <main>
-        <div>
-<button type="submit" id="logoutbutton" style="float: right;">Abmelden</button>
+        <div  >
+<button type="submit" id="logoutbutton" style="float: right; margin-right:50px; margin-top: 10px;">Abmelden</button>
         </div>    
     </main>
-</form>    
-<br></br>
-<br></br>
-<h1>Herzlich Willkommen!</h1> 
-<br></br>
+</form> 
 
-<br>
-<br>
+
+ <blockquote style="border-style: solid; border-color:#b5b5b5; border-width: 1px;"> 
+<!-- admin portal zur studienplanung als überschrift,
+upload button name in importieren -->
+<figure> <img src="https://upload.wikimedia.org/wikipedia/de/9/90/Hochschule_f%C3%BCr_Wirtschaft_und_Recht_Berlin_logo.svg" width="604" height="120"
+	alt="Logo">
+	</figure>
+<br></br>
+<div class="title" style="margin-left:450px ;margin-right:50px; margin-top: 10px; color= red;">
+<div class="ü1" style="margin-left: 26px; color:#6B6F73;">
+<h1>Herzlich Willkommen!</h1> 
+</div>
+<div class="ü2" style="margin-left: 5px; color:#d30000;">
+<h2>Admin-Portal für die Lehrplanung</h2>
+</div>
+<br></br>
+</div>
+
+ </blockquote>  
+ 
+ 
+<blockquote style="border-style: solid; border-color:#b5b5b5; border-width: 1px;">
+<br> </br>
+
 <!-- Datei auswählen Button -->
 	<form  method="post" enctype="multipart/form-data">
-    Select CSV file to upload and create JSON files from:
+    <!-- Select CSV file to upload and create JSON files from: -->
     <input type="file" name="fileToUpload" id="fileToUpload">
+	<div class="menu" style="margin-left:10px;">
 	<p>Letzer Vorlesungstag : <input type="date" name="end_lecture_time_input" /></p>
 	<p>Start Vorlesungspause : <input type="date" name="lecture_free_time_start_input" /></p>
 	<p>Ende Vorlesungspause : <input type="date" name="lecture_free_time_end_input" /></p>
-    <input type="submit" value="Upload CSV" name="submit">
+	</div>
+	<div class="buttons" style="margin-left:10px;">
+	<div class="b2" style="margin-left: -7px;">
+    <input type="submit" value="Importieren" name="submit">
+	</div>
 	</form>
-
-  
-  <br>
-  <br>
-  
-<!-- <form action="admin-portal-v2.php" method="post">
-   <input type="submit" name="vorschau" value="Vorschau"/>
-
-</form>
--->
+	</div>
+<blockquote>
+<br><br>
 <br>
-   <a href='../fullcalendar/start3.php'><button>Kalender anzeigen</button></a>
 
-<?php
-#$selectedValue = $_POST['Auswahl'];
-?>  
+<input type="button" id="test_start" value='Show Layer' onclick="javascript:toggle();";>
 
+<div id="test" style="display: none;">
+	<?php
+		include 'admin_calendar.php';
+	?>
+	
+	<form  method="post" enctype="multipart/form-data">
+	<select id="publish" style="font-size: 16px;background-color:#dc1010;color:white;font-family: Arial,sans-serif;font-weight: bold;margin-top:120px;">
+                    <option value="Semester" data-feed=""selected>Semester wählen</option>
+                    <?php
+					#https://www.tutdepot.com/create-a-select-menu-from-files-or-directory/					
+					$dir2 = "events/";
+					$handle2 = opendir($dir2);
+					while (false !== ($file2 = readdir($handle2))) {
+						$files2[] = $file2;
+					}
+					closedir($handle2);
+					sort($files2);	
+					
+
+					#TODO: Hier Aussortierung von Dateien, welche nicht mehr notwendig sind
+					#das heißt: keine Datei von alten Semester
+					foreach($files2 as $entry){
+						if(is_file($dir2.$entry)){
+							$entry_year = array();
+							preg_match('/[0-9][0-9][0-9][0-9]/', $entry ,$entry_year);
+							$entry_year = $entry_year[0];							
+							
+							$entry_sem = array();
+							preg_match('/[0-9][.]/', $entry ,$entry_sem);						
+							$entry_sem = $entry_sem[0];
+							$entry_sem = $entry_sem+0;
+							
+							$entry_major = array();
+							preg_match('/[A-Z][A-Z]/', $entry ,$entry_major);
+							$entry_major = $entry_major[0];
+							
+							#Entry_Year != Year (Bsp: 2017 != 2018) ODER Entry_Year != Jetzt-3Monate (Bsp: es ist März 2019, es gilt aber weiter 2018 ungerade Semester)
+							if($entry_year != date("Y") OR $entry_year != date("Y", time()-7889400)){
+								$key = array_search($entry,$files2);
+								if($key!==false){
+									unset($files2[$key]);
+								}
+							}
+							
+							#Oktober bis März => lösche gerade Semester
+							if(date("n")>= 10 OR date("n")<4){
+								#TODO: Hier falsch gecodet fürs Beispiel!!! == auf != ändern
+								if($entry_sem %2 != 0){
+									$key = array_search($entry,$files2);
+									if($key!==false){
+										unset($files2[$key]);
+									}
+								}	
+							}
+							#April bis September => lösche ungerade Semester
+							else if(date("n")>= 4 && date("n")<10){
+								if($entry_sem %2 != 0){
+									$key = array_search($entry,$files2);
+									if($key!==false){
+										unset($files2[$key]);
+									}
+								}	
+							}
+						}
+					}
+					
+					$counter = 0;
+					foreach ($files2 as $val) {
+						if (is_file($dir2.$val)) { // show only "real" files2
+							$mydir2 .= '
+							<option value="'.$val.'" data-feed="'.$dir2.$val.'"';
+							
+							#TODO: Hier Namensanpassung
+							#Schema: VI_2018_2.json
+							
+							$name = "";
+							
+							$val_year = array();
+							preg_match('/[0-9][0-9][0-9][0-9]/', $val ,$val_year);
+							$val_year = $val_year[0];							
+							
+							$val_sem = array();
+							preg_match('/[0-9][.]/', $val ,$val_sem);						
+							$val_sem = $val_sem[0];
+							$val_sem = $val_sem+0;
+							
+							$val_major = array();
+							preg_match('/[A-Z][A-Z]/', $val ,$val_major);
+							$val_major = $val_major[0];
+							
+							if($val_major == "VI"){
+								$name .= "Verwaltungsinformatik ";
+							}
+							
+							$name .= $val_sem.". Semester ".$val_year;
+							
+							
+							$mydir2 .= '>'.$name.'</option>';
+							$counter++;
+						}
+					}					
+					echo $mydir2;
+					?>
+		</select>
+		<input type="submit" value="Freigebenen" name="start_puplish">
+</div>
+
+<script language="javascript">
+
+    function toggle(){
+
+        var ele = document.getElementById("test");
+
+        if (ele.style.display == "block") {
+
+            ele.style.display = "none";
+        }
+        else {
+
+            ele.style.display = "block";
+        }
+    }	
+		
+	
+	<?php
+			if(isset($_POST['start_puplish'])){
+				rename("events/".$_POST['publish'], "public/".$_POST['publish']);
+			}
+	?>
+</script>
 
 <?php
 
@@ -112,13 +252,7 @@
 			echo '<script type="text/javascript">alert("Sorry, there was an error uploading your file.")</script>';
         }
       } 
-    }	
-	
-	if(isset($_POST['vorschau'])){
-		
-		global $events_dir;
-		echo select_files($events_dir, 'vorschau_menu', 'Vorhandene Event-Dateien: ', 30);
-	}
+    }
 	
 	#https://www.tutdepot.com/create-a-select-menu-from-files-or-directory/
 	function select_files($dir, $select_name, $label = '', $curr_val = '', $mlength = 30) {
@@ -173,6 +307,32 @@
 	
 ?>
 
+<style>
 
+/** Hintergrund  **/
+body {
+	background-color: #fefefe ;}
+
+blockquote{blockquote {
+   padding: 5px;
+   margin-top: 5px;
+   width: 80%;
+   margin-left: auto;
+   margin-right: auto;
+   margin-bottom: 1.2em;
+   font-family: UnBatang, FreeSerif, Georgia, serif;
+   font-size: 1.2em;
+   background-color: white;
+}
+blockquote i {
+   color: blue;
+}
+
+title{
+	text-align: center;
+}
+
+	
+</style>
 </body>
 <html>
---
+++ admin-portal/admin_calendar.php
@@ -0,0 +1,195 @@
+<html>
+<?php
+	#include 'php/create_ics.php';
+?>
+    <body>
+		<span>Verwaltungsinformatik:</span>
+                <!dropdown-menu
+                <!fürs dropdown muss noch ne eigene css-formatierung angelegt werden!>
+		<select id="dropdown" style="font-size: 16px;background-color:#dc1010;color:white;font-family: Arial,sans-serif;font-weight: bold;margin-top:120px;">
+                    <option value="Semester" data-feed=""selected>Semester wählen</option>
+                    <?php
+					#https://www.tutdepot.com/create-a-select-menu-from-files-or-directory/					
+					$dir = "events/";
+					$handle = opendir($dir);
+					while (false !== ($file = readdir($handle))) {
+						$files[] = $file;
+					}
+					closedir($handle);
+					sort($files);	
+					
+
+					#TODO: Hier Aussortierung von Dateien, welche nicht mehr notwendig sind
+					#das heißt: keine Datei von alten Semester
+					foreach($files as $entry){
+						if(is_file($dir.$entry)){
+							$entry_year = array();
+							preg_match('/[0-9][0-9][0-9][0-9]/', $entry ,$entry_year);
+							$entry_year = $entry_year[0];							
+							
+							$entry_sem = array();
+							preg_match('/[0-9][.]/', $entry ,$entry_sem);						
+							$entry_sem = $entry_sem[0];
+							$entry_sem = $entry_sem+0;
+							
+							$entry_major = array();
+							preg_match('/[A-Z][A-Z]/', $entry ,$entry_major);
+							$entry_major = $entry_major[0];
+							
+							#Entry_Year != Year (Bsp: 2017 != 2018) ODER Entry_Year != Jetzt-3Monate (Bsp: es ist März 2019, es gilt aber weiter 2018 ungerade Semester)
+							if($entry_year != date("Y") OR $entry_year != date("Y", time()-7889400)){
+								$key = array_search($entry,$files);
+								if($key!==false){
+									unset($files[$key]);
+								}
+							}
+							
+							#Oktober bis März => lösche gerade Semester
+							if(date("n")>= 10 OR date("n")<4){
+								#TODO: Hier falsch gecodet fürs Beispiel!!! == auf != ändern
+								if($entry_sem %2 != 0){
+									$key = array_search($entry,$files);
+									if($key!==false){
+										unset($files[$key]);
+									}
+								}	
+							}
+							#April bis September => lösche ungerade Semester
+							else if(date("n")>= 4 && date("n")<10){
+								if($entry_sem %2 != 0){
+									$key = array_search($entry,$files);
+									if($key!==false){
+										unset($files[$key]);
+									}
+								}	
+							}
+						}
+					}
+					
+					$counter = 0;
+					foreach ($files as $val) {
+						if (is_file($dir.$val)) { // show only "real" files
+							$mydir .= '
+							<option value="'.$val.'" data-feed="'.$dir.$val.'"';
+							
+							#TODO: Hier Namensanpassung
+							#Schema: VI_2018_2.json
+							
+							$name = "";
+							
+							$val_year = array();
+							preg_match('/[0-9][0-9][0-9][0-9]/', $val ,$val_year);
+							$val_year = $val_year[0];							
+							
+							$val_sem = array();
+							preg_match('/[0-9][.]/', $val ,$val_sem);						
+							$val_sem = $val_sem[0];
+							$val_sem = $val_sem+0;
+							
+							$val_major = array();
+							preg_match('/[A-Z][A-Z]/', $val ,$val_major);
+							$val_major = $val_major[0];
+							
+							if($val_major == "VI"){
+								$name .= "Verwaltungsinformatik ";
+							}
+							
+							$name .= $val_sem.". Semester ".$val_year;
+							
+							
+							$mydir .= '>'.$name.'</option>';
+							$counter++;
+						}
+					}					
+					echo $mydir;
+					?>
+		</select>
+                    
+		<!css ist unten zu finden - postionierung etc.!>
+		<div id="calendar"></div>				
+				
+    </body>
+    
+    <head>
+                <!nicht sicher ,ob alles gebraucht wird!>
+		<link rel="stylesheet" href="../fullcalendar/css/jquery-ui.min.css">
+        <link rel='stylesheet' href='../fullcalendar/fullcalendar.css' />
+		<script src='../fullcalendar/lib/jquery.min.js'></script>
+		<script src='../fullcalendar/lib/moment.min.js'></script>                       
+		<script src='../fullcalendar/fullcalendar.js'></script>
+                <script src='../fullcalendar/locale/de.js'></script>
+                <script src="../fullcalendar/external/jquery/jquery.js"></script>
+                <script src="../fullcalendar/jquery-ui.min.js"></script>
+		<script>
+                    
+                 //Variable selectedfeed,um einzelne selected json Dateien zu benutzen   
+		var selectedFeed = $('#dropdown').find(':selected').data('feed');
+
+                //Calendar-start
+                $('#calendar').fullCalendar({
+                
+                    locale: 'de',
+                    lang: 'de',
+                    editable: false,
+                    firstDay: 1,
+                    displayEventTime: false,
+                    //Hier werden die einzelnen Events nicht wie bisher durch eine genaue .json aufgerufen ,sondern durch mein selectedFeed (aus dem dropdown)
+                    eventSources: [ selectedFeed ],
+                    eventLimit: 5,
+                   //hab ein Theme (Blitzer)benutzt von https://jqueryui.com/themeroller/
+                    themeSystem: 'jquery-ui',
+                    header: {
+                        left: 'prev,next today',
+                        center: 'title',
+                        right: 'month,agendaWeek,agendaDay,listWeek'
+                    },
+                });
+
+            //mit Hilfe aus :https://stackoverflow.com/questions/45794528/fullcalendar-event-filter-select-almost-working letzter Aufruf:13.11.2018 19:35
+
+            $('#dropdown').change(onSelectChangeFeed);
+                
+                //Funktion um das seleltierte Semester als events zu öffen
+                function onSelectChangeFeed() { 
+                
+		var feed = $(this).find(':selected').data('feed');
+                
+		$('#calendar').fullCalendar('removeEventSource', selectedFeed);
+                
+                $('#calendar').fullCalendar('addEventSource', feed);  
+                
+                //Übertragung feed auf selected feed, welches bei Eventsource benutzt wird
+                selectedFeed = feed;
+                };
+				
+		function download(d){
+			if(d == 'Semester2') return;
+			window.location = d;
+		}
+		</script>
+	
+                <style>
+            
+		  #script-warning {
+			display: none;
+			background: #eee;
+			border-bottom: 1px solid #ddd;
+			padding: 0 10px;
+			line-height: 40px;
+			text-align: center;
+			font-weight: bold;
+			font-size: 12px;
+			color: red;
+		  }
+		  
+		  
+		  #calendar {
+                        position: relative;
+			max-width: 1200px;
+			margin: auto;
+			padding: 0 10px;     
+		  }
+                  
+		</style>
+        </head>	
+</html> 
