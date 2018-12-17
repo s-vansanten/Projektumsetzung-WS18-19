14dc5c133bfc4b3c0fa547f5a4e28b3589eeba04
lelahelao <44228309+lelahelao@users.noreply.github.com>
2018-12-17 10:20:33 +0100

    Add files via upload

---
+++ admin-portal/admin-portal-login-3.php
@@ -0,0 +1,97 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<link href='style.css' rel='stylesheet' />
+<title>Admin-Portal der HWR</title>
+
+<script defer src="js/script.js"></script>
+
+ <blockquote style="border-style: solid; border-color:#b5b5b5; border-width: 1px; background-color:white"> 
+
+<link rel="shortcut icon" href="//moodle.hwr-berlin.de/pluginfile.php/1/theme_boost_campus/favicon/1539771992/favicon.png" />
+</head>
+<body>
+<figure> <img src="https://upload.wikimedia.org/wikipedia/de/9/90/Hochschule_f%C3%BCr_Wirtschaft_und_Recht_Berlin_logo.svg" width="604" height="120"
+	alt="Logo">
+	</figure>
+	</blockquote>
+
+
+ <blockquote style="border-style: solid; border-color:#b5b5b5; border-width: 1px; background-color:white">	
+ <div class="menu" style="margin-left:20px;color:#4A4D50;">
+	<h1>Admin-Portal der Hochschule f&uumlr Wirtschaft und Recht</h1>
+	<br>
+	
+<main>
+
+<div class="menu" style="margin-left:10px;">
+	<table>
+		<tbody>
+			<tr>
+				<th>
+						
+					<label for="username" class="sr-only">
+                                            
+                                   </label>
+						
+				</th>
+				<td>
+					<input type="text" name="username" id="username"
+                                          class="form-control"
+                                          value=""
+                                           placeholder="Anmeldename">
+						
+			</tr>
+			<tr>
+				<th>
+						
+					<label for="password" class="sr-only"></label>
+						
+				</th>
+				<td>
+					<input type="password" name="password" id="password" value=""
+                                        class="form-control"
+                                        placeholder="Kennwort">						
+			</tr>
+			<tr>
+				<td> </td>
+				<td>
+					<button type="submit" id="loginbutton" onclick="pruefe()">Anmelden</button>
+				</td>
+			</tr>
+		</tbody>
+	</table>
+	<br>
+	<br>
+</div>
+</blockquote>
+</main>
+
+<style>
+
+/** Hintergrund  **/
+body {
+	background-color: #F0F0F1 ;}
+
+blockquote{blockquote {
+   padding: 5px;
+   margin-top: 5px;
+   width: 80%;
+   margin-left: auto;
+   margin-right: auto;
+   margin-bottom: 1.2em;
+   font-family: UnBatang, FreeSerif, Georgia, serif;
+   font-size: 1.2em;
+   background-color: white;
+}
+blockquote i {
+   color: blue;
+}
+
+title{
+	text-align: center;
+}
+	
+</style>
+</body>
+<html>
