76b50f4eb6da6b46d4dece75971309237e22ca9e
Judith <s_braesecke@stud.hwr-berlin.de>
2018-12-18 23:10:03 +0100

    Create admin-portal-login-presentation.php

---
+++ admin-portal/admin-portal-login-presentation.php
@@ -0,0 +1,122 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<link href='style.css' rel='stylesheet' />
+<title>Admin-Portal der HWR</title>
+
+<script defer src="js/script.js"></script>
+
+ <div id="blockquote"> 
+
+<link rel="shortcut icon" href="//moodle.hwr-berlin.de/pluginfile.php/1/theme_boost_campus/favicon/1539771992/favicon.png" />
+</head>
+<body>
+<figure> <img src="https://upload.wikimedia.org/wikipedia/de/9/90/Hochschule_f%C3%BCr_Wirtschaft_und_Recht_Berlin_logo.svg" width="15%" height="15%"
+	alt="Logo">
+	</figure>
+    <div class="titleüberschriften" style="margin-left:450px ;margin-right:50px; margin-top: 10px; color:red;">
+    <div id="ü1" style="margin-left: 5%; color:#6B6F73;">
+<h1>Login!</h1> 
+</div>
+    <div id="ü2" style="margin-left: 0%; color:#d30000;">
+<h2>Admin-Portal für die Lehrplanung</h2>
+</div>
+    </div>
+	</div>
+
+
+ <div id= "blockquote2">	
+ <div class="menu" style="margin-left:20px;color:#4A4D50;">
+	
+	<br>
+	
+<main>
+
+<div class="menu" style="margin-left:10px;">
+	<table>
+		<tbody>
+			<tr>
+				<th>
+						
+					<label for="username" class="sr-only">
+                                            
+                                   </label>
+						
+				</th>
+				<td>
+					<input type="text" name="username" id="username"
+                                          class="form-control"
+                                          value=""
+                                           placeholder="Anmeldename">
+						
+			</tr>
+			<tr>
+				<th>
+						
+					<label for="password" class="sr-only"></label>
+						
+				</th>
+				<td>
+					<input type="password" name="password" id="password" value=""
+                                        class="form-control"
+                                        placeholder="Kennwort">						
+			</tr>
+			<tr>
+				<td> </td>
+				<td>
+					<button type="submit" id="loginbutton" onclick="pruefe()">Anmelden</button>
+				</td>
+			</tr>
+		</tbody>
+	</table>
+	<br>
+	<br>
+</div>
+</div>
+</main>
+</body>
+<style>
+
+/** Hintergrund  **/
+#body {
+	background-color: #F0F0F1;
+}
+
+#blockquote {
+   padding: 5px;
+   margin-top: 5px;
+   width: 85%;
+   margin-left: auto;
+   margin-right: auto;
+   margin-bottom: 1.2em;
+   font-family: Crimson Text Roman;
+   font-size: 0.7em;
+   background-color:#FAFAFA;
+  
+}
+#ü1{
+    position:absolute;
+    bottom:91%;
+}
+#ü2{
+    position:absolute;
+    bottom:88%;
+}
+#blockquote2{
+    font-family: Crimson Text Roman;
+    width: 30%;
+    background-color:#FAFAFA;
+    margin-left: 7.2%;
+   margin-right: auto;
+}
+blockquote i {
+   color: blue;
+}
+
+title{
+	text-align: center;
+}
+	
+</style>
+
+<html>
