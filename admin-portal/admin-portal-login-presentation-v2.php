c7ef716b1bb250566a4543c3d6e2ce25c396c605
lelahelao <44228309+lelahelao@users.noreply.github.com>
2019-01-01 16:47:07 +0100

    Add files via upload

---
+++ admin-portal/admin-portal-login-presentation-v2.php
@@ -0,0 +1,77 @@
+
+<!DOCTYPE HTML>
+<html>
+<head>
+
+<link href='style/loginstyle.css' rel="stylesheet" />
+<title>Admin-Portal der HWR</title>
+
+<script defer src="js/script.js"></script>
+
+
+<div id="blockquote3">
+ <!-- <blockquote style="border-style: solid; border-color:#b5b5b5; border-width: 1px; background-color:white"> -->
+
+<link rel="shortcut icon" href="//moodle.hwr-berlin.de/pluginfile.php/1/theme_boost_campus/favicon/1539771992/favicon.png" />
+</head>
+<body>
+<figure> <img src="https://upload.wikimedia.org/wikipedia/de/9/90/Hochschule_f%C3%BCr_Wirtschaft_und_Recht_Berlin_logo.svg" width="604" height="120"
+	alt="Logo">
+	</figure>
+	<br><br><br>
+	<br><br><br>
+	<br><br><br>
+	</div><!-- </blockquote> -->
+<div id="blockquote4">
+ <!-- <blockquote style="border-style: solid; border-color:#b5b5b5; border-width: 1px; background-color:white">	-->
+ <div class="menu1">
+	<h1>Admin-Portal der Hochschule f&uumlr Wirtschaft und Recht</h1>
+	<br>
+	
+<main>
+
+<div class="menu2" >
+	<table>
+		<tbody>
+			<tr>
+				<th>
+						
+					<label for="username" class="sr-only">
+                                            
+                                   </label>
+						
+				</th>
+				<td>
+					<input type="text" name="username" id="username"
+                                          class="form-control"
+                                          value=""
+                                           placeholder="Anmeldename">
+						
+			</tr>
+			<tr>
+				<th>
+						
+					<label for="password" class="sr-only"></label>
+						
+				</th>
+				<td>
+					<input type="password" name="password" id="password" value=""
+                                        class="form-control"
+                                        placeholder="Kennwort">						
+			</tr>
+			<tr>
+				<td> </td>
+				<td>
+					<button type="submit" id="loginbutton" onclick="pruefe()">Anmelden</button>
+				</td>
+			</tr>
+		</tbody>
+	</table>
+	<br>
+	<br>
+</div>
+<!-- </blockquote> -->
+</div>
+</main>
+</body>
+<html>
