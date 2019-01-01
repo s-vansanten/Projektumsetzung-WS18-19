bee838393a9052d50c16fe028dd41b0a61989307
lelahelao <44228309+lelahelao@users.noreply.github.com>
2019-01-01 20:25:41 +0100

    Update start_presentation.php

--- fullcalendar/start_presentation.php
+++ fullcalendar/start_presentation.php
@@ -1,6 +1,8 @@
-﻿<html>
+
+<html>
     <head>
-  
+<link href='css/startstyle.css' rel='stylesheet' />
+
 	<div id="box1"> 
 	<figure id="logopic"> <img src="https://upload.wikimedia.org/wikipedia/de/9/90/Hochschule_f%C3%BCr_Wirtschaft_und_Recht_Berlin_logo.svg" width="604" height="120"
 	alt="Logo">        
@@ -26,7 +28,6 @@
 					}
 					closedir($handle);
 					sort($files);
-
 					#TODO: Hier Ansortierung von Dateien, welche nicht mehr notwendig sind
 					#das heißt: keine Datei von alten Semester
 					
@@ -127,12 +128,11 @@
  
 </div>
   
-             <span id="auswahl" style="font-size:20px;cursor:pointer"  onclick="openNav()">&#9776; Auswahl</span>
+             <span id="auswahl" onclick="openNav()">&#9776; Auswahl</span>
              <script>
 function openNav() {
   document.getElementById("mySidenav").style.width = "300px";
 }
-
 function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
 }
@@ -162,7 +162,6 @@ function closeNav() {
                     
                  //Variable selectedfeed,um einzelne selected json Dateien zu benutzen   
 		var selectedFeed = $('#dropdown').find(':selected').data('feed');
-
                 //Calendar-start
                $('#calendar').fullCalendar({
                 
@@ -189,9 +188,7 @@ function closeNav() {
                         right: 'month,agendaWeek,agendaDay,listWeek'
                     },
                 });
-
             //mit Hilfe aus :https://stackoverflow.com/questions/45794528/fullcalendar-event-filter-select-almost-working letzter Aufruf:13.11.2018 19:35
-
             $('#dropdown').change(onSelectChangeFeed);
                 
                 //Funktion um das seleltierte Semester als events zu öffen
@@ -209,238 +206,6 @@ function closeNav() {
 		</script>
                  
 
-                <style>
-            
-		          /**?**/
-		  #script-warning {
-			display: none;
-			background: #eee;
-			border-bottom: 1px solid #ddd;
-			padding: 0 10px;
-			line-height: 40px;
-			text-align: center;
-			font-weight: bold;
-			font-size: 12px;
-			color: red;
-		  }
-		  
-		  
-	 
-		  /**Kalender**/
-		  #calendar {
-                        position: relative;
-                      font-family:Crimson Text Roman;
-						max-width:800px;
-						margin: 80px;
-						padding: 0 10px;  
-						background-color: white;
-						border-style: solid;
-						border-color: #979A9D;
-						opacity: 0,5;
-                                                right:-8%;
-                                                 vertical-align:middle;
-                                                 color:#4A4D50;
-                                                 
-}
-body .fc {
-   font-family: Crimson Text Roman;  
-}
-.ui-button{
-    font-family: Crimson Text Roman; 
-    font-weight: lighter;
-}
-.fc-content{
-   
-           max-height:40px;
-}
- .fc-hover {
-    max-height:none!important; 
-}
-.fc-title{
-         white-space: normal;
-        
-   }
-   .fc-event {
-   /** float: left;**/
-    font-size: 0.6em;   
-    font-family:Crimson Text Roman;
- 
-}
-
-
-
-						
-		  
-		  
-		  /**Logo**/
-		logo{
-			background-image:url(../Test/images/logo.svg);
-			background-repeat:no-repeat;
-			background-size:100% auto;
-			background-position:center center;
-			width:40%;
-			height:80%;
-			position:absolute;
-			left:80%;
-			top:0;
-			display:block; 
-			
-			
-			}
-			
-
-
-/** Hintergrund Webseite **/
-body {
-	background-color:#F0F0F1 ;
-        width:100%;
-height:100%;
-	}
-	
-	
-
-	#box1 {
-	background-color:#FAFAFA;
-	margin-top: 0px;
-	margin-left: 10%;
-	width: 1170px;
-	height: 1500px;
-       
-} 
-#logopic{
-    position: relative;
-    left:8%;
-}
-		
-	/**Dropdown Überschrift**/
-#modulname{
-     position: absolute;
-     left:-20%;
-     bottom:69%;
-     color:#4A4D50;
-     font-family: Crimson Text Roman;
-	font-weight: normal;
-	margin-left:25%;
-       text-decoration: underline overline;
-       
-}
-#dropdown {
-    bottom:65%;
-     position: absolute;
-     left:5%;
-     background-color:#FFFFFF;
-     color:#4A4D50;
-     font-size:0.7em;
-    
-}
-#navigationlink{
-         color:#4A4D50;
-   left:5%;
-  bottom:50%;
-     position: absolute; 
-     text-decoration: underline overline;
-    
-
-}
-#ahwr{
-           color:#4A4D50;
-   left:-6%;
-  bottom:42%;
-     position: absolute; 
-      font-size:1em;    
-}
-#amoodle{
-           color:#4A4D50;
-   left:-6%;
-  bottom:45%;
-     position: absolute; 
-      font-size:1em;
-     
-}
-
-#downloadspan{
-         color:#4A4D50;
-   left:5%;
-  bottom:13.5%;
-     position: absolute;      
-}
-    #download{
-            left:65%;
-         bottom:13.5%;
-     position: absolute;
-    color: #4A4D50; 
-    }
-#download2spezi{
-    color:#4A4D50;
-   left:5%;
-  bottom:10%;
-     position: absolute;      
-}
-#download2{
-    left:65%;
-         bottom:10%;
-     position: absolute;
-    color: #4A4D50;
-}
-#navinnen {
-   background-color:#FFFFFF; 
-   bottom:54.5%;
-   position: absolute;
-   width:80%;
-   left:9%;
-}
-
-#auswahl {
-    margin: 4px 4px;
-    left: 13%;
-    bottom: 65%;
-    position: absolute;
-    color:#4A4D50;
-}
-.sidenav {
-  height: 100%;
-  width: 0;
-  position: fixed;
-  z-index: 1;
-  top: 0;
-  left: 0;
-  background-color: #F0F0F1;
-  overflow-x: hidden;
-  transition: 0.5s;
-  padding-top: 60px;
- 
-}
-
-.sidenav a {
-  padding: 8px 8px 8px 32px;
-  text-decoration: none;
-  font-size: 25px;
-  color: #818181;
-  display: block;
-  transition: 0.3s;
-}
-
-.sidenav a:hover {
-  color: #f1f1f1;
-}
-
-.sidenav .closebtn {
-  position: absolute;
-  top: 0;
-  right: 25px;
-  font-size: 36px;
-  margin-left: 50px;
-}
-
-@media screen and (max-height: 450px) {
-  .sidenav {padding-top: 15px;}
-  .sidenav a {font-size: 18px;}
-}
-
-/* was angepasst werden muss : obere hellgraue Rand, hellgraue Rand so breit wie der Kalender, 
-Kalender Borderlininen grau, in der funktion bearbeiten und css ausgeliedern
-Gewünscht wurde noch : dropdown nach links verschieben, farben der Buttons ändern*/
-    
-		</style>
+              
         </head>	
 </html> 
